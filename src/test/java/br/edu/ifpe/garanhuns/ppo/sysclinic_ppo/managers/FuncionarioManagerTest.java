9698b624fbaa514eebb80f0eafa29025f07c18f2
KatarinaLopes <katarinalopesc@gmail.com>
2018-01-31 10:48:59 -0300

    Testado ḿétodo validar em FuncionarioManager

--- src/test/java/br/edu/ifpe/garanhuns/ppo/sysclinic_ppo/managers/FuncionarioManagerTest.java
+++ src/test/java/br/edu/ifpe/garanhuns/ppo/sysclinic_ppo/managers/FuncionarioManagerTest.java
@@ -5,18 +5,25 @@
  */
 package br.edu.ifpe.garanhuns.ppo.sysclinic_ppo.managers;
 
+import br.edu.ifpe.garanhuns.ppo.sysclinic_ppo.models.business.Funcionario;
+import br.edu.ifpe.garanhuns.ppo.sysclinic_ppo.models.persistence.dao.DaoFuncionario;
+import br.edu.ifpe.garanhuns.ppo.sysclinic_ppo.models.persistence.dao.manager.DaoGenerico;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  *
  * @author Katarina
  */
 public class FuncionarioManagerTest {
+    
+    private FuncionarioManager funcionarioManager;
+    private DaoGenerico daoFuncionarios;
     
     public FuncionarioManagerTest() {
     }
@@ -31,6 +38,9 @@ public class FuncionarioManagerTest {
     
     @Before
     public void setUp() {
+        daoFuncionarios = mock(DaoFuncionario.class);
+        funcionarioManager = new FuncionarioManager
+        ((DaoFuncionario) daoFuncionarios);
     }
     
     @After
@@ -38,8 +48,74 @@ public class FuncionarioManagerTest {
     }
 
     @Test
-    public void testValidar() {
+    public void deveTestarValidarPassandoNoTeste() {
+        String senha = "123";
+        String confirmacaoSenha = "123";
+        int matricula = 1234;
+        
+        Funcionario funcionario = new Funcionario();
+        
+        funcionario.setMatricula(matricula);
+        funcionario.setSenha(senha);
+        
+        when(daoFuncionarios.recuperarPorAtributo("matricula", matricula)).
+                thenReturn(null);
+        
+        funcionarioManager.validar(funcionario, confirmacaoSenha);
+        
     }
+    
+    @Test
+    public void deveTestarValidarSenhasDiferentesFalhando(){
+        String senha = "123";
+        String confirmacaoSenha = "1234";
+        int matricula = 1234;
+        String mensagem = "";
+        
+        Funcionario funcionario = new Funcionario();
+        
+        funcionario.setMatricula(matricula);
+        funcionario.setSenha(senha);
+        
+        when(daoFuncionarios.recuperarPorAtributo("matricula", matricula)).
+                thenReturn(null);
+        
+        try{
+            funcionarioManager.validar(funcionario, confirmacaoSenha);
+            fail();
+        }catch(IllegalArgumentException ex){
+            mensagem = ex.getMessage();
+        }
+        
+        assertEquals("As senhas não correspondem!", mensagem);
+    }
+    
+    @Test
+    public void deveTestarValidarPacienteExistenteFalhando(){
+        String senha = "123";
+        String confirmacaoSenha = "123";
+        int matricula = 1234;
+        String mensagem = "";
+        
+        Funcionario funcionario = new Funcionario();
+        
+        funcionario.setMatricula(matricula);
+        funcionario.setSenha(senha);
+        
+        when(daoFuncionarios.recuperarPorAtributo("matricula", matricula)).
+                thenReturn(funcionario);
+        
+        try{
+            funcionarioManager.validar(funcionario, confirmacaoSenha);
+            fail();
+        }catch(IllegalArgumentException ex){
+            mensagem = ex.getMessage();
+        }
+        
+        assertEquals("Este funcionário já está cadastrado, verifique se a "
+                + "matrícula está correto", mensagem);
+    }
+
 
     @Test
     public void testCadastrar() throws Exception {
