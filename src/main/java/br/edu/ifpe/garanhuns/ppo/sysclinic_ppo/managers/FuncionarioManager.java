a4707e7f3dd74f2fa9d2b44920e55f3440e68f32
KatarinaLopes <katarinalopesc@gmail.com>
2018-02-01 02:57:11 -0300

    Classe FuncionarioManager refatorada, com javadoc, faltam testes com CRUD

--- src/main/java/br/edu/ifpe/garanhuns/ppo/sysclinic_ppo/managers/FuncionarioManager.java
+++ src/main/java/br/edu/ifpe/garanhuns/ppo/sysclinic_ppo/managers/FuncionarioManager.java
@@ -14,20 +14,46 @@ import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 /**
- *
+ * EN-US
+ * Class to manage all operations related to Funcionario
+ * 
+ * PT-BR
+ * Classe para gerenciar todas as operações relacionadas a Funcionario
+ * 
  * @author Katarina
  */
 public class FuncionarioManager {
-    private DaoGenerico daoFuncionarios;
+    private final DaoGenerico daoFuncionarios;
     
     public FuncionarioManager(DaoFuncionario daoFuncionarios){
         this.daoFuncionarios = daoFuncionarios;
     }
     
-    public void validar(Funcionario funcionario, String senha){
+    /**
+     * EN-US
+     * Method to validate the given Funcionario, if the passwords are equal and
+     * if there's already a Funcionario with the same matricula in DB
+     * If the passwords aren't equal or there's already a Funcionario 
+     * registered, a IllegalArgumentException will be thrown
+     * 
+     * 
+     * PT-BR
+     * Método para validar o Funcionario dado, se as senha são iguais e se já
+     * existe um Funcionario com a mesma matrícula no BD
+     * Se as senhas não são iguais ou já tem um Funcionario cadastrado, uma
+     * IllegalArgumentException será lançada
+     * 
+     * @param funcionario representing the Funcionario to be validated | 
+     * representando o funcionário para ser validado
+     * @param senhaConfirmacao representing the password to be compared | 
+     * representando a senha que será comparada
+     * @throws IllegalArgumentException
+     */
+    public void validar(Funcionario funcionario, String senhaConfirmacao) 
+            throws IllegalArgumentException{
         String senhaFuncionario = funcionario.getSenha();
 
-        if (!senhaFuncionario.equals(senha)) {
+        if (!senhaFuncionario.equals(senhaConfirmacao)) {
             throw new IllegalArgumentException("As senhas não correspondem!");
         }
        
@@ -40,28 +66,86 @@ public class FuncionarioManager {
         }
     }
     
-    public void cadastrar(Funcionario funcionario, String confirmacaoSenha) 
-            throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    /**
+     * EN-US
+     * Validates the Funcionario and then persists it
+     * 
+     * PT-BR
+     * Valida o Funcionario e depois o persiste
+     * @param funcionario representing the Funcionario to be persisted | 
+     * representando o funcionario ser persistido
+     * @param confirmacaoSenha representing the password to be compared | 
+     * representando a senha ser comparada
+     */
+    public void cadastrar(Funcionario funcionario, String confirmacaoSenha) {
         validar(funcionario, confirmacaoSenha);
         daoFuncionarios.persistir(funcionario);
     }
     
+    /**
+     * EN-US
+     * Retrieves a Funcionario from DB with the given id
+     * 
+     * PT-BR
+     * Recupera um funcionario do BD com a id dada
+     * @param id
+     * @return a Funcionario, if exists, null if it doesn't | um Funcionario, 
+     * se existir, null se não
+     */
     public Funcionario recuperar(int id){
         return (Funcionario) daoFuncionarios.recuperar(id);
     }
     
-    public void atualizar(Funcionario f){
-        daoFuncionarios.atualizar(f);
+    /**
+     * EN-US
+     * Updates the given Funcionario
+     * 
+     * PT-BR
+     * Atualiza o funcionario dado
+     * @param funcionario representing the Funcionario | representando o 
+     * funcionario
+     */
+    public void atualizar(Funcionario funcionario){
+        daoFuncionarios.atualizar(funcionario);
     }
     
-    public void deletar(Funcionario f){
-        daoFuncionarios.deletar(f);
+    /**
+     * EN-US
+     * Deletes the given Funcionario from the DB
+     * 
+     * PT-BR
+     * Deleta o funcionario dado do BD
+     * @param funcionario representing the Funcionario | representando o 
+     * funcionario
+     */
+    public void deletar(Funcionario funcionario){
+        daoFuncionarios.deletar(funcionario);
     }
     
+    /**
+     * EN-US
+     * Retrieves all Funcionario from DB
+     * 
+     * PT-BR
+     * Recupera todos os Funcionario do BD
+     * @return all Funcionario | todos os Funcionario
+     */
     public List<Funcionario> recuperarTodos(){
         return daoFuncionarios.recuperarTodos();
     }
     
+    /**
+     * EN-US
+     * Verifies if there's more than one Funcionario, according to it's 
+     * privilege (if Administrador or not)
+     * 
+     * PT-BR
+     * Verifica se existe mais de um Funcionario, de acordo com seu privilégio
+     * (se administrador ou não)
+     * @param administrador representing the status | representando o status
+     * @return true if there is, false if there isn't | true se tem, false se 
+     * não tem
+     */
     public boolean existeMaisDeUm(boolean administrador){
         int qtdeFuncionario = 0;
         int qtdeAdministrador = 0;
@@ -83,6 +167,16 @@ public class FuncionarioManager {
         }
     }
     
+    /**
+     * EN-US
+     * Returns an alert message about changing a Funcionario privilege
+     * 
+     * PT-BR
+     * Retorna uma mensagem de alerta sobre mudar o privilégio de um 
+     * Funcionario
+     * 
+     * @return an alert message | uma mensagem de alerta
+     */
     public String retornarMensagemDeAlerta(){
         return "Administradores podem cadastrar, alterar e excluir "
                         + "médicos e funcionários, mas não têm acesso "
--- src/test/java/br/edu/ifpe/garanhuns/ppo/sysclinic_ppo/managers/FuncionarioManagerTest.java
+++ src/test/java/br/edu/ifpe/garanhuns/ppo/sysclinic_ppo/managers/FuncionarioManagerTest.java
@@ -8,6 +8,8 @@ package br.edu.ifpe.garanhuns.ppo.sysclinic_ppo.managers;
 import br.edu.ifpe.garanhuns.ppo.sysclinic_ppo.models.business.Funcionario;
 import br.edu.ifpe.garanhuns.ppo.sysclinic_ppo.models.persistence.dao.DaoFuncionario;
 import br.edu.ifpe.garanhuns.ppo.sysclinic_ppo.models.persistence.dao.manager.DaoGenerico;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -119,6 +121,156 @@ public class FuncionarioManagerTest {
 
     @Test
     public void testCadastrar() throws Exception {
+    }
+
+    @Test
+    public void testValidar() {
+    }
+
+    @Test
+    public void testRecuperar() {
+    }
+
+    @Test
+    public void testAtualizar() {
+    }
+
+    @Test
+    public void testDeletar() {
+    }
+
+    @Test
+    public void testRecuperarTodos() {
+    }
+
+    @Test
+    public void deveTestarExisteMaisDeUmAdministradorRetornandoTrue() {
+        Funcionario funcionario = new Funcionario();
+        Funcionario funcionarioAdmin = new Funcionario();
+        funcionarioAdmin.setAdministrador(true);
+        
+        List funcionarios = new ArrayList();
+        
+        funcionarios.add(funcionario);
+        funcionarios.add(funcionarioAdmin);
+        funcionarios.add(funcionarioAdmin);
+        
+        when(daoFuncionarios.recuperarTodos()).thenReturn(funcionarios);
+        
+        boolean retorno = funcionarioManager.existeMaisDeUm(true);
+        
+        assertTrue(retorno);
+        
+    }
+    
+    @Test
+    public void deveTestarExisteMaisDeUmAdministradorRetornandoFalse() {
+        Funcionario funcionario = new Funcionario();
+        Funcionario funcionarioAdmin = new Funcionario();
+        funcionarioAdmin.setAdministrador(true);
+        
+        List funcionarios = new ArrayList();
+        
+        funcionarios.add(funcionario);
+        funcionarios.add(funcionarioAdmin);
+        funcionarios.add(funcionario);
+        
+        when(daoFuncionarios.recuperarTodos()).thenReturn(funcionarios);
+        
+        boolean retorno = funcionarioManager.existeMaisDeUm(true);
+        
+        assertFalse(retorno);
+        
+    }
+    
+    @Test
+    public void deveTestarExisteMaisDeUmAdministrador1DeCadaRetornandoFalse() {
+        Funcionario funcionario = new Funcionario();
+        Funcionario funcionarioAdmin = new Funcionario();
+        funcionarioAdmin.setAdministrador(true);
+        
+        List funcionarios = new ArrayList();
+        
+        funcionarios.add(funcionario);
+        funcionarios.add(funcionarioAdmin);
+        
+        when(daoFuncionarios.recuperarTodos()).thenReturn(funcionarios);
+        
+        boolean retorno = funcionarioManager.existeMaisDeUm(true);
+        
+        assertFalse(retorno);
+        
+    }
+    
+    @Test
+    public void deveTestarExisteMaisDeUmFuncionarioRetornandoTrue() {
+        Funcionario funcionario = new Funcionario();
+        Funcionario funcionarioAdmin = new Funcionario();
+        funcionarioAdmin.setAdministrador(true);
+        
+        List funcionarios = new ArrayList();
+        
+        funcionarios.add(funcionario);
+        funcionarios.add(funcionario);
+        funcionarios.add(funcionarioAdmin);
+        
+        when(daoFuncionarios.recuperarTodos()).thenReturn(funcionarios);
+        
+        boolean retorno = funcionarioManager.existeMaisDeUm(false);
+        
+        assertTrue(retorno);
+        
+    }
+    
+    @Test
+    public void deveTestarExisteMaisDeUmFuncionarioRetornandoFalse() {
+        Funcionario funcionario = new Funcionario();
+        Funcionario funcionarioAdmin = new Funcionario();
+        funcionarioAdmin.setAdministrador(true);
+        
+        List funcionarios = new ArrayList();
+        
+        funcionarios.add(funcionario);
+        funcionarios.add(funcionarioAdmin);
+        funcionarios.add(funcionarioAdmin);
+        
+        when(daoFuncionarios.recuperarTodos()).thenReturn(funcionarios);
+        
+        boolean retorno = funcionarioManager.existeMaisDeUm(false);
+        
+        assertFalse(retorno);
+        
+    }
+    
+    @Test
+    public void deveTestarExisteMaisDeUmFuncionario1DeCadaRetornandoFalse() {
+        Funcionario funcionario = new Funcionario();
+        Funcionario funcionarioAdmin = new Funcionario();
+        funcionarioAdmin.setAdministrador(true);
+        
+        List funcionarios = new ArrayList();
+        
+        funcionarios.add(funcionario);
+        funcionarios.add(funcionarioAdmin);
+        
+        when(daoFuncionarios.recuperarTodos()).thenReturn(funcionarios);
+        
+        boolean retorno = funcionarioManager.existeMaisDeUm(false);
+        
+        assertFalse(retorno);
+        
+    }
+
+    @Test
+    public void testRetornarMensagemDeAlerta() {
+        String message = funcionarioManager.retornarMensagemDeAlerta();
+        
+        String mensagemCorreta = "Administradores podem cadastrar, alterar e "
+                + "excluir médicos e funcionários, mas não têm acesso ao "
+                + "sistema de agendamentos. Mude o privilégio apenas se "
+                + "necessário";
+        
+        assertEquals(mensagemCorreta, message);
     }
     
 }
